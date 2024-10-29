f2c8169281ab7f2ee012379c61ff68f0e0e572d3
Ana Kelry <anakelry346@gmail.com>
2024-10-29 15:08:15 -0300

    slakakakakak

--- BiblioVirtual/src/DadosBiblio.java
+++ BiblioVirtual/src/DadosBiblio.java
@@ -3,9 +3,21 @@ import java.util.List;
 
 public class DadosBiblio {
 
+    
+    private DadosBiblio(){}
+    private static DadosBiblio instance;
+    
+    
     private List<Livro> listaLivros = new ArrayList<>();
     private List<Editora> listaEditora = new ArrayList<>();
     private List<Categoria> listaCategoria = new ArrayList<>();
     private List<Autor> listaAutor = new ArrayList<>();
-        
+    
+    public static DadosBiblio getInstance(){
+        if(instance == null){
+            instance = new DadosBiblio();
+        }
+        return instance;
+    }
+
 }
