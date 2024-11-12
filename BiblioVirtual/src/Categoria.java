e32e422a2ab94b5c106b6568191eddf0dbc7117a
GabrielCode22 <118139244+GabrielCode22@users.noreply.github.com>
2024-11-12 19:36:15 -0300

    Add files via upload

--- BiblioVirtual/src/Categoria.java
+++ BiblioVirtual/src/Categoria.java
@@ -1,16 +1,45 @@
-
-
 public class Categoria {
-
-    
     private int idCategoria;
     private String nomeCategoria;
-    private String descriçaoCategoria;
+    private String descricaoCategoria;
 
-    public void editarCategoria(){
-         //edita as infos da categoria
+    private static Categoria instancia;
+
+    private Categoria() {}
+
+    public static Categoria getInstancia() {
+        if (instancia == null) {
+            instancia = new Categoria();
+        }
+        return instancia;
     }
-    
-   }
-   
-   
+
+    public int getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(int idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getNomeCategoria() {
+        return nomeCategoria;
+    }
+
+    public void setNomeCategoria(String nomeCategoria) {
+        this.nomeCategoria = nomeCategoria;
+    }
+
+    public String getDescricaoCategoria() {
+        return descricaoCategoria;
+    }
+
+    public void setDescricaoCategoria(String descricaoCategoria) {
+        this.descricaoCategoria = descricaoCategoria;
+    }
+
+    public void editarCategoria(String nome, String descricao) {
+        this.nomeCategoria = nome;
+        this.descricaoCategoria = descricao;
+    }
+}
